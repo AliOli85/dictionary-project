6d4461ef06766158b6fc966f4ba2ff274742d1aa
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-07-15 15:45:44 +0200

    Added search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+          Coded by Alicia
+        </small>
+      </footer>
     </div>
   );
 }
